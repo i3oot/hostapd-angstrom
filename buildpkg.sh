1d50f968813a957b840d802fe81aea4629a7cc6c
root <root@beaglebone.(none)>
2014-08-16 11:12:45 +0000

    install hostapd to current dir

---
+++ buildpkg.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+opkg install wget libnl-3-dev
+wget http://w1.fi/releases/hostapd-2.2.tar.gz
+tar zxvf hostapd-2.2.tar.gz
+cd hostapd-2.2/hostapd
+cp defconfig .config
+sed -r -i "s/#CONFIG_LIBNL32=y/CONFIG_LIBNL32=y/" .config
+make
+export DESTDIR=`pwd`
+make install
+
+
