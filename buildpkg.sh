9c7b24d5d8cb154994c46a0433da4f7fc08e5944
root <root@beaglebone.(none)>
2014-08-16 11:52:14 +0000

    fixes for buildscript

--- buildpkg.sh
+++ buildpkg.sh
@@ -3,11 +3,13 @@
 opkg install wget libnl-3-dev
 wget http://w1.fi/releases/hostapd-2.2.tar.gz
 tar zxvf hostapd-2.2.tar.gz
-cd hostapd-2.2/hostapd
+export DESTDIR=`pwd`/build
+pushd hostapd-2.2/hostapd
 cp defconfig .config
 sed -r -i "s/#CONFIG_LIBNL32=y/CONFIG_LIBNL32=y/" .config
 make
-export DESTDIR=`pwd`/build
+mkdir -p $DESTDIR/usr/local/bin
 make install
+popd
 ./ipkg-build build .
 
